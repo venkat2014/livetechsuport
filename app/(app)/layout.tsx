0842e127a00aa236598bc2a88774c1be5c9fc6ec
venkat2014 <41752298+venkat2014@users.noreply.github.com>
2025-11-07 02:19:13 +0530

    Update layout.tsx

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -12,31 +12,16 @@ export default async function Layout({ children }: LayoutProps) {
   return (
     <>
       <header className="fixed top-0 left-0 z-50 hidden w-full flex-row justify-between p-6 md:flex">
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://livekit.io"
-          className="scale-100 transition-transform duration-300 hover:scale-110"
-        >
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src={logo} alt={`${companyName} Logo`} className="block size-6 dark:hidden" />
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src={logoDark ?? logo}
-            alt={`${companyName} Logo`}
-            className="hidden size-6 dark:block"
-          />
-        </a>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={logo} alt={`${companyName} Logo`} className="block size-6 dark:hidden" />
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src={logoDark ?? logo}
+          alt={`${companyName} Logo`}
+          className="hidden size-6 dark:block"
+        />
         <span className="text-foreground font-mono text-xs font-bold tracking-wider uppercase">
-          Built with{' '}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://docs.livekit.io/agents"
-            className="underline underline-offset-4"
-          >
-            LiveKit Agents
-          </a>
+          © 2025 LiveTechSupport
         </span>
       </header>
 
